9afe9a64525d9be71a8c427bbab092518fe0476d
adc688 <1460899486@qq.com>
2023-04-22 12:34:15 -0400

    Initial backend created

---
+++ server/routes/api/posts.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongodb = require('mongodb');
+const router = express.Router();
+
+// GET posts
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+//add post
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+//delete post
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://alice123:alice123@pr3.0mi5hac.mongodb.net/?retryWrites=true&w=majority',{
+        useNewUrlParser: true
+    });
+
+    return client.db('vue_express').collection('posts');
+}
+
+module.exports = router;
